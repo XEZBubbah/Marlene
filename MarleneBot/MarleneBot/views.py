5132be9da92e91f8356c1aa6fda5556477a0cdd2
RecursiveDeveloper <63681573+RecursiveDeveloper@users.noreply.github.com>
2020-10-29 16:51:20 -0500

    version 1.0 subproceso

--- MarleneBot/MarleneBot/views.py
+++ MarleneBot/MarleneBot/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
-
+import subprocess
 
 def principal_view(request):
 
+    subproceso = subprocess.Popen(
+        ['C:/Users/Lenovo/Documents/GitHub/Marlene/MarleneBot/redNeuronal/ChatBot_Algorithm>python3 chat.py'], 
+        stdout=subprocess.PIPE,
+        shell=True
+    )
+    salida, error = subproceso.communicate()
+    print(salida)
+    print("\n")
+    print(type(salida))
     return render(request,'./principal.html')
 
